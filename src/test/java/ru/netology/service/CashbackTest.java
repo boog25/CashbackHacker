69137272f03c09464f713796d1122f27eedaa6f8
boog25 <voroshilov.d.v123@gmail.com>
2021-02-27 18:11:29 +0500

    init

--- src/test/java/ru/netology/service/CashbackTest.java
+++ src/test/java/ru/netology/service/CashbackTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackTest {
     @Test
@@ -10,7 +10,7 @@ public class CashbackTest {
         Cashback service = new Cashback();
         int actual = service.remain(900);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -18,7 +18,7 @@ public class CashbackTest {
         Cashback service = new Cashback();
         int actual = service.remain(1001);
         int expected = 999;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,7 +26,7 @@ public class CashbackTest {
         Cashback service = new Cashback();
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -34,6 +34,6 @@ public class CashbackTest {
         Cashback service = new Cashback();
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
